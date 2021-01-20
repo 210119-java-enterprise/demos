6af0ab53626eaea38023283bcde0ab1272a0e9d2
Calvin Zheng <calvin.zheng@revature.net>
2021-01-20 11:45:42 -0600

    Added helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+//only one top-level (public) class is allowed
+//name of this class must match the file name exactly
+public class helloworld {
+    //main method
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+
+
+}
