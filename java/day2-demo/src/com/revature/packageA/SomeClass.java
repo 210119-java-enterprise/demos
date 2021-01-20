e7957d13c9e13bcb2ee60143af31fae3a25cb45d
wezley <wezley.singleton@gmail.com>
2021-01-20 18:10:35 -0500

    pushing up day2 demo

---
+++ java/day2-demo/src/com/revature/packageA/SomeClass.java
@@ -0,0 +1,12 @@
+package com.revature.packageA;
+
+public class SomeClass {
+
+    public int x = 10;
+    protected char c = 'c';
+    float f = 3.14f;
+
+    // only accessible inside of this class declaration
+    private double d = 2.77;
+
+}
