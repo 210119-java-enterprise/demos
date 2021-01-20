2bb5319b0823b5f52b662ca97d99b953e3924370
Moody Almukhtar <moodyx1993@gmail.com>
2021-01-20 09:47:41 -0800

    adding HelloWorld from yesterday

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// only ont-top (public) class is allowed
+// only one top-level (public) class is allowed
+public class HelloWorld {
+
+	// main method (entry point of all java applications)
+	public static void main(String[] args) {
+		system.out.println("Hello, world!");
+
+	}
+}
+
+// legal because its not public, but kinda weird, probably wont do this often
+class AnotherClass {
+
+}
