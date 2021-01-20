5b344dfa7796c5eba65142cacb002d54d543eed1
0xSyn <31738750+0xSyn@users.noreply.github.com>
2021-01-20 11:30:13 -0600

    helloworld.java

---
+++ java/HelloWorld.java
@@ -0,0 +1,13 @@
+//only 1 top level (public) class is allowed
+//name must match filename
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
+
+
+//Legal (b/c not public)
+class AnotherClass{
+	
+}
