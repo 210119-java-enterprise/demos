3d0da1a07257ecde6348810efc391d879ad3845d
kml160030 <kalyb369@hotmail.com>
2021-01-20 22:59:12 -0600

    pushing up day2demo

---
+++ Java/Day2demo/src/com/revature/packageA/SomeClass.java
@@ -0,0 +1,10 @@
+package com.revature.packageA;
+
+public class SomeClass {
+    public int x = 10;
+    protected char c = 'c';
+    float f = 3.14f;
+
+    // only accessible inside of this class declaration
+    //private double = 2.77;
+}
