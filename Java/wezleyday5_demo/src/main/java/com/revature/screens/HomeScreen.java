65fd79449f55e3d5da666cd8204e3bb6759b1bf8
unknown <alex.googe@revature.net>
2021-01-25 15:42:59 -0500

    added HomeScreen

---
+++ Java/wezleyday5_demo/src/main/java/com/revature/screens/HomeScreen.java
@@ -0,0 +1,46 @@
+package com.revature.screens;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+
+public class HomeScreen extends Screen {
+
+    public HomeScreen() {
+        super("HomeScreen", "/home");
+    }
+
+    @Override
+    public void render() {
+        System.out.println("Welcome to Decrypter!\n");
+        System.out.println("1) Login");
+        System.out.println("2) Register");
+        System.out.println("3) Exit Application");
+
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.println(">");
+            String userSelection = consoleReader.readLine();
+
+            switch (userSelection) {
+                case "1":
+                    System.out.println("Navigating to Login Screen.");
+                    break;
+                case "2":
+                    System.out.println("Navigating to Register Screen.");
+                    break;
+                case "3":
+                    System.out.println("Exiting Application");
+                    break;
+                default:
+                    System.out.println("Invalid selection!");
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
