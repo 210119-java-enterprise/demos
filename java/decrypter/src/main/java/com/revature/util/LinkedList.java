38e0aa87daac846e0ffce1578bdcf2ed8f919712
wezley <wezley.singleton@gmail.com>
2021-01-25 16:20:18 -0500

    setup for group activity

--- java/decrypter/src/main/java/com/revature/util/LinkedList.java
+++ java/decrypter/src/main/java/com/revature/util/LinkedList.java
@@ -1,4 +1,49 @@
 package com.revature.util;
 
-public class LinkedList {
+public class LinkedList<T> {
+
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
+    public void insert(T data) {
+
+    }
+
+    public T getFirst() {
+        return null;
+    }
+
+    public void deleteFirst() {
+
+    }
+
+    public boolean contains(T data) {
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printList() {
+
+    }
+
+    private static class Node<T> {
+        T data;
+        Node<T> nextNode;
+        Node<T> prevNode;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+        Node(T data, Node<T> nextNode, Node<T> prevNode) {
+            this(data);
+            this.nextNode = nextNode;
+            this.prevNode = prevNode;
+        }
+    }
+
 }
