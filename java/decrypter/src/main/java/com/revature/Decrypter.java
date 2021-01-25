b8e076c62aba03d3db669dc2a201cbded50f2af0
wezley <wezley.singleton@gmail.com>
2021-01-25 16:11:48 -0500

    boilerplate, will refactor

--- java/decrypter/src/main/java/com/revature/Decrypter.java
+++ java/decrypter/src/main/java/com/revature/Decrypter.java
@@ -1,12 +1,19 @@
 package com.revature;
 
 import com.revature.screens.HomeScreen;
+import com.revature.util.AppState;
 
 public class Decrypter {
+
+    private static AppState app = new AppState();
 
     public static void main(String[] args) {
         HomeScreen homeScreen = new HomeScreen();
         homeScreen.render();
+    }
+
+    public static AppState app() {
+        return app;
     }
 
 }
