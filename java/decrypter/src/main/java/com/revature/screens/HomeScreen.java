063dbb997f11057bdf51a899c5249647ad19b0fd
wezley <wezley.singleton@gmail.com>
2021-01-25 15:42:19 -0500

    basic HomeScreen impl

---
+++ java/decrypter/src/main/java/com/revature/screens/HomeScreen.java
@@ -0,0 +1,51 @@
+package com.revature.screens;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class HomeScreen extends Screen {
+
+    public HomeScreen() {
+        super("HomeScreen", "/home");
+    }
+
+    @Override
+    public void render() {
+
+        System.out.println("Welcome to Decrypter!\n");
+        System.out.println("1) Login");
+        System.out.println("2) Register");
+        System.out.println("3) Exit application");
+
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+
+            System.out.println("> ");
+            String userSelection = consoleReader.readLine();
+
+            switch (userSelection) {
+                case "1":
+                    System.out.println("Navigating to login screen");
+                    break;
+                case "2":
+                    System.out.println("Navigating to register screen");
+                    break;
+                case "3":
+                    System.out.println("Exiting application");
+                    break;
+                default:
+                    System.out.println("Invalid selection!");
+
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+}
