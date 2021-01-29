f274489c921a269b4cf28e7c2450c0e88e5b7234
Brandon Johnson <brandon.johnson@revature.net>
2021-01-29 09:00:56 -0600

    drycrypt proj caught up with class

--- MyStuff/decrypter/src/main/java/com/revature/repos/CrudRepository.java
+++ MyStuff/decrypter/src/main/java/com/revature/repos/CrudRepository.java
@@ -3,9 +3,11 @@ package com.revature.repos;
 import com.revature.util.Set;
 
 public interface CrudRepository<T> {
+
     void save(T newObj);
     Set<T> findAll();
     T findById(int id);
-    boolean update(T updateObj);
+    boolean update(T updatedObj);
     boolean deleteById(int id);
+
 }
--- MyStuff/decrypter/src/main/java/com/revature/screens/RegisterScreen.java
+++ MyStuff/decrypter/src/main/java/com/revature/screens/RegisterScreen.java
@@ -2,35 +2,43 @@ package com.revature.screens;
 
 import com.revature.models.AppUser;
 
-public class RegisterScreen extends Screen{
+import static com.revature.Decrypter.app;
 
-    public RegisterScreen(){
-        super("RegisterScreen","/register");
+public class RegisterScreen extends Screen {
+
+    public RegisterScreen() {
+        super("RegisterScreen", "/register");
     }
 
     @Override
     public void render() {
+
         String firstName;
         String lastName;
         String username;
         String password;
 
         try {
-            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n+--------------------------------------+\nRegistration Screen");
-            System.out.println("Sign up for a free account");
-            System.out.println("First Name: ");
-            firstName = app().getConsole.readLine();
-            System.out.println("Last Name: ");
-            lastName = app().getConsole.readLine();
-            System.out.println("Username: ");
-            username = app().getConsole.readLine();
-            System.out.println("Password: ");
-            password = app().getConsole.readLine();
 
-            AppUser newUser =new AppUser(firstName,lastName,username,password);
-        }catch(Exception e){
+            System.out.println("\n\n\n\n\n\n\n\n\n\n+---------------------+");
+            System.out.println("Sign up for a new account!");
+            System.out.print("First name: ");
+            firstName = app().getConsole().readLine();
+            System.out.print("Last name: ");
+            lastName = app().getConsole().readLine();
+            System.out.print("Username: ");
+            username = app().getConsole().readLine();
+            System.out.print("Password: ");
+            password = app().getConsole().readLine();
+
+            AppUser newUser = new AppUser(firstName, lastName, username, password);
+
+            // pass our new user to a service class that will validate it and then pass it on
+            // to a DAO that will attempt to persist it, we'll get something back to let us
+            // know how to navigate from here.
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 }
