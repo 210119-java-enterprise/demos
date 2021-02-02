8bb5cd6dcbf70a7f9c75ca1f7c478eaabf3b498d
ECorpron <ecorpron@gmail.com>
2021-02-02 08:53:45 -0800

    added more notes

--- Java/FunctionalJava/src/main/java/com/revature/Animal.java
+++ Java/FunctionalJava/src/main/java/com/revature/Animal.java
@@ -3,5 +3,5 @@ package com.revature;
 public abstract class Animal {
 
     public abstract void eat();
-    public abstract void makeSound();
+//    public abstract void makeSound();
 }
--- Java/FunctionalJava/src/main/java/com/revature/Driver.java
+++ Java/FunctionalJava/src/main/java/com/revature/Driver.java
@@ -4,6 +4,8 @@ public class Driver {
 
     public static void main(String[] args) {
 
+        // Local anonymous class
+        // Can be an inline implementation of any abstract type (interface or abstract class)
         Animal newAnimal = new Animal() {
 
             @Override
@@ -11,13 +13,42 @@ public class Driver {
                 System.out.println("slurp");
             }
 
-            @Override
-            public void makeSound() {
-                System.out.println("waaakaakaa");
-            }
+//            @Override
+//            public void makeSound() {
+//                System.out.println("waaakaakaa");
+//            }
         };
 
+        // can't use lambdas with things that aren't functional interfaces
+//        Animal wontWork = () -> {
+//
+//        }
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("This is a local anonymous class that implements the Runnable interface");
+            }
+        })
+
+        Thread t2 = new Thread(() -> {
+            System.out.println("This is a lambda expression that implements the Runnable interface.");
+        });
+
+        t2.start();
+
         newAnimal.eat();
-        newAnimal.makeSound();
+//        newAnimal.makeSound();
+
+        /*
+            Legal Lambda Expression Syntax
+                - (parameter) -> expression
+                - parameter -> expression
+                - parameter -> { code block of expressions }
+                - (parameter, parameter) -> expression
+                - (parameter, parameter) -> {code block of expressions}
+                - () -> expression
+                - () -> { code block of expressions }
+         */
     }
 }
