a22b4d2742ffd9e6824541a3f1611320bb7906ad
wezley <wezley.singleton@gmail.com>
2021-02-04 15:50:07 -0500

    added ClassInspector placeholder and impl User domain obj

---
+++ reflection/src/main/java/com/revature/model/User.java
@@ -0,0 +1,82 @@
+package com.revature.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private List<String> features;
+
+    public User() {
+        super();
+    }
+
+    public User(int id, String username, String password, List<String> features) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.features = features;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<String> features) {
+        this.features = features;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(username, user.username) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(features, user.features);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, features);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", features=" + features +
+                '}';
+    }
+
+}
