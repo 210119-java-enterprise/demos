94c002ed2de822760fe657ce867f370e8af31bf1
wezley <wezley.singleton@gmail.com>
2021-02-10 11:23:40 -0500

    added a proof of concept for ORM metamodel; and moved reflection project to proper dir

---
+++ java/reflection/src/main/java/com/revature/orm/OrmDriver.java
@@ -0,0 +1,27 @@
+package com.revature.orm;
+
+import com.revature.orm.models.User;
+import com.revature.orm.util.ColumnField;
+import com.revature.orm.util.IdField;
+import com.revature.orm.util.Metamodel;
+
+import java.util.List;
+
+public class OrmDriver {
+
+    public static void main(String[] args) {
+
+        Metamodel<User> userMetamodel = Metamodel.of(User.class);
+
+        IdField idField = userMetamodel.getPrimaryKey();
+        List<ColumnField> columnFields = userMetamodel.getColumns();
+
+        System.out.printf("The primary key of User is: %s\n", idField.getName());
+
+        for (ColumnField columnField : columnFields) {
+            System.out.printf("The User class contains a column called: %s\n", columnField.getName());
+        }
+
+    }
+
+}
